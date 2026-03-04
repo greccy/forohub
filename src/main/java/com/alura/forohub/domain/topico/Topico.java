9155e1a45cd943d273eeed4fe44ffe272756aa07
greccy <greccy.verde@gmail.com>
2026-03-04 22:19:59 +0100

    Add: Iniciando app

---
+++ src/main/java/com/alura/forohub/domain/topico/Topico.java
@@ -0,0 +1,58 @@
+package com.alura.forohub.domain.topico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table(name = "topicos")
+@Entity(name = "Topico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Topico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String mensaje;
+
+    @Column(name = "fecha_creacion")
+    private LocalDateTime fechaCreacion;
+
+    @Enumerated(EnumType.STRING)
+    private TopicoStatus status;
+
+    private String autor;
+    private String curso;
+
+    public Topico(DatosRegistroTopico datos) {
+        this.titulo = datos.titulo();
+        this.mensaje = datos.mensaje();
+        this.autor = datos.autor();
+        this.curso = datos.curso();
+        this.fechaCreacion = LocalDateTime.now();
+        this.status = TopicoStatus.ABIERTO;
+    }
+
+    public void actualizar(DatosActualizacionTopico datos) {
+        if (datos.titulo() != null) {
+            this.titulo = datos.titulo();
+        }
+        if (datos.mensaje() != null) {
+            this.mensaje = datos.mensaje();
+        }
+        if (datos.status() != null) {
+            this.status = datos.status();
+        }
+        if (datos.curso() != null) {
+            this.curso = datos.curso();
+        }
+    }
+}
